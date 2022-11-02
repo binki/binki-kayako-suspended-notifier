33b47e7dbf4b79b011b3dd4183902f9c54b07a18
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2022-11-02 14:02:11 -0400

    Add support for automatically deleting suspended messages matching regular expressions.
    Fixes #3.

--- binki-kayako-suspended-notifier.user.js
+++ binki-kayako-suspended-notifier.user.js
@@ -6,6 +6,9 @@
 // @match https://*.kayako.com/agent/*
 // @require https://github.com/binki/binki-userscript-delay-async/raw/252c301cdbd21eb41fa0227c49cd53dc5a6d1e58/binki-userscript-delay-async.js
 // @require https://github.com/binki/binki-userscript-when-element-changed-async/raw/88cf57674ab8fcaa0e86bdf5209342ec7780739a/binki-userscript-when-element-changed-async.js
+// @grant GM.getValue
+// @grant GM.registerMenuCommand
+// @grant GM.setValue
 // ==/UserScript==
 
 (async () => {
@@ -23,14 +26,35 @@
     centerDiv.style.backgroundColor = '#000';
     centerDiv.style.padding = '8pt';
     centerDiv.style.border = '1pt solid silver';
+    centerDiv.style.maxWidth = '100%';
     div.appendChild(centerDiv);
     document.body.appendChild(div);
     await actionAsync(centerDiv);
     div.parentElement.removeChild(div);
   };
+  const autoDeleteValueKey = 'autoDelete';
+  let autoDelete = await GM.getValue(autoDeleteValueKey, '');
   const permission = await navigator.permissions.query({
     name: 'notifications',
   });
+  GM.registerMenuCommand('Binki Suspended Notifier Settings', async () => {
+    await dialogAsync(async centerDiv => {
+      const div = document.createElement('div');
+      centerDiv.appendChild(div);
+      div.textContent = 'binki-kayako-suspended-notifier: List of auto-delete regular expressions. Each line is treated as a single regular expression. The data matched against the regular expression is the Suspension Reason (e.g., LOOP_DETECTED) followed by a space followed by the email address.';
+      const textArea = document.createElement('textarea');
+      textArea.value = autoDelete;
+      centerDiv.appendChild(textArea);
+      const button = document.createElement('button');
+      button.textContent = 'Save';
+      button.type = 'button';
+      centerDiv.appendChild(button);
+      await new Promise(resolve => button.addEventListener('click', resolve));
+      button.disabled = true;
+      await GM.setValue(autoDeleteValueKey, textArea.value);
+      autoDelete = textArea.value;
+    });
+  }, 's');
   while (true) {
     while (permission.state !== 'granted') {
       await dialogAsync(async centerDiv => {
@@ -81,6 +105,7 @@
         'Authorization': 'X-Do-Not-Prompt-User',
       },
     });
+    let refreshNeeded = false;
     const fetchResult = await (async () => {
       try {
       	return await fetch(request);
@@ -92,24 +117,86 @@
       }
     })();
     if (fetchResult.status === 200) {
-      const totalCount = (await fetchResult.json()).total_count;
-      if (typeof totalCount !== 'number') {
-        throw new Error(`Unhandled data schema.`);
+      const data = await fetchResult.json();
+      for (const email of data.data) {
+        const autoDeleteKey = `${email.suspension_code} ${email.from}`;
+        const matchesAutoDelete = await (async () => {
+          for (const line of autoDelete.split(/[\r\n]/g)) {
+            if (line) {
+              const lineRegExp = await (async () => {
+                try {
+                  return new RegExp(line);
+                } catch (ex) {
+                  const notification = new Notification(`Error parsing regular expression`, {
+                    requireInteraction: true,
+                    tag: `kayako-suspended-notifier-regexp-error`,
+                  });
+                  notification.addEventListener('click', e => {
+                    dialogAsync(async centerDiv => {
+                      centerDiv.textContent = `error parsing regular expression “${lineRegExp}”: ${ex}`;
+                      const button = document.createElement('button');
+                      button.type = 'button';
+                      button.textContent = 'OK';
+                      centerDiv.appendChild(button);
+                      await new Promise(resolve => button.addEventListener('click', resolve));
+                    });
+                  });
+                }
+              })();
+              if (lineRegExp && lineRegExp.test(autoDeleteKey)) {
+                console.log(`autoDelete match (${line}): ${autoDeleteKey}`);
+                return true;
+              }
+            }
+          }
+          return false;
+        })();
+        if (matchesAutoDelete) {
+          const request = new Request(new URL(`/api/v1/mails/${encodeURIComponent(email.id)}?include=*`, document.location.href).href, {
+            headers: {
+              'Authorization': 'X-Do-Not-Prompt-User',
+              'X-CSRF-Token': JSON.parse(localStorage['ko:core:csrf']),
+            },
+            method: 'DELETE',
+          });
+          const fetchResult = await (async () => {
+            try {
+              return await fetch(request);
+            } catch (ex) {
+              console.log(`Unable to auto delete suspended ticket ${email.id}`, ex);
+              return {
+                status: 0,
+              };
+            }
+          })();
+          if (fetchResult.status === 200) {
+            console.log(`Automatically deleted suspended message ${email.id}`);
+            refreshNeeded = true;
+          } else {
+            console.log('Faled to delete suspended message', fetchResult);
+          }
+        }
       }
-      if (totalCount) {
-        const notification = new Notification(`${totalCount} Suspended Messages`, {
-          requireInteraction: true,
-          tag: 'kayako-suspended-notifier',
-        });
-        notification.addEventListener('click', e => {
-          document.querySelector('a[href*=suspended-messages]').click();
-        });
+      if (!refreshNeeded) {
+        const totalCount = data.total_count;
+        if (typeof totalCount !== 'number') {
+          throw new Error(`Unhandled data schema.`);
+        }
+        if (totalCount !== 0) {
+          const notification = new Notification(`${totalCount} Suspended Messages`, {
+            requireInteraction: true,
+            tag: 'kayako-suspended-notifier',
+          });
+          notification.addEventListener('click', e => {
+            document.querySelector('a[href*=suspended-messages]').click();
+          });
+        }
       }
     } else {
       // Likely some issue. Could be transient.
       console.log('Checking for suspended tickets failed.', fetchResult);
     }
     // Don’t check too frequently. Every 5 minutes is probably more than sufficient.
-    await delayAsync(5*60*1000);
+    if (!refreshNeeded) await delayAsync(5*60*1000);
   }
 })();
